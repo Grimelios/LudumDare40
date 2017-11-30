254be2886a0dbabc56033fe61bedbd55d0382fbe
Mark <grimelios@gmail.com>
2017-11-29 23:49:33 -0600

    Adding HUD classes.

---
+++ LudumDare40/UI/Hud/HeadsUpDisplay.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LudumDare40.Interfaces;
+using LudumDare40.Json;
+using Microsoft.Xna.Framework;
+
+namespace LudumDare40.UI.Hud
+{
+	public class HeadsUpDisplay : IDynamic, IRenderable
+	{
+		private List<HudElement> elements;
+		
+		public HeadsUpDisplay()
+		{
+			elements = new List<HudElement>();
+		}
+		
+		public void Load(string filename)
+		{
+			elements.Clear();
+			elements.AddRange(JsonUtilities.Deserialize<HudElement[]>(filename, true));
+
+			foreach (HudElement element in elements)
+			{
+				Alignments alignment = element.Alignment;
+
+				int offsetX = element.OffsetX;
+				int offsetY = element.OffsetY;
+				int x = (alignment & Alignments.Left) == Alignments.Left
+					? offsetX
+					: (alignment & Alignments.Right) == Alignments.Right
+						? Resolution.Width - offsetX
+						: Resolution.Width / 2 + offsetX;
+
+				int y = (alignment & Alignments.Top) == Alignments.Top
+					? offsetY
+					: (alignment & Alignments.Bottom) == Alignments.Bottom
+						? Resolution.Height - offsetY
+						: Resolution.Height / 2 + offsetY;
+
+				element.Position = new Vector2(x, y);
+				element.Initialize();
+			}
+		}
+		
+		public T GetElement<T>() where T : HudElement
+		{
+			return (T)elements.FirstOrDefault(e => e is T);
+		}
+		
+		public void Update(float dt)
+		{
+			elements.ForEach(e =>
+			{
+				if (e.Visible)
+				{
+					e.Update(dt);
+				}
+			});
+		}
+		
+		public void Draw(SuperBatch sb)
+		{
+			elements.ForEach(e =>
+			{
+				if (e.Visible)
+				{
+					e.Draw(sb);
+				}
+			});
+		}
+	}
+}
---
+++ LudumDare40/UI/Hud/HudElement.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LudumDare40.Core;
+using Microsoft.Xna.Framework;
+using Newtonsoft.Json;
+
+namespace LudumDare40.UI.Hud
+{
+	public abstract class HudElement : Container2D
+	{
+		protected HudElement()
+		{
+			KeyboardEnabled = false;
+			MouseEnabled = false;
+		}
+		
+		[JsonProperty]
+		public Alignments Alignment { get; set; }
+		
+		[JsonProperty]
+		public int OffsetX { get; set; }
+		
+		[JsonProperty]
+		public int OffsetY { get; set; }
+		
+		public virtual void Initialize()
+		{
+		}
+	}
+}
