ef4117eebc6c52c007677f6c95e3d12060e4bdcf
Mark <grimelios@gmail.com>
2017-11-29 22:12:59 -0600

    Initial commit.

---
+++ LudumDare40/MainGame.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace LudumDare40
+{
+	public class MainGame : Game
+	{
+		private GraphicsDeviceManager graphics;
+		private SpriteBatch spriteBatch;
+
+		public MainGame()
+		{
+			graphics = new GraphicsDeviceManager(this);
+			Content.RootDirectory = "Content";
+			Window.Title = "Ludum Dare 40";
+			Window.Position = new Point(150);
+			IsMouseVisible = true;
+		}
+		
+		protected override void Initialize()
+		{
+		}
+		
+		protected override void LoadContent()
+		{
+			spriteBatch = new SpriteBatch(GraphicsDevice);
+		}
+		
+		protected override void UnloadContent()
+		{
+		}
+		
+		protected override void Update(GameTime gameTime)
+		{
+		}
+		
+		protected override void Draw(GameTime gameTime)
+		{
+			GraphicsDevice.Clear(Color.Black);
+		}
+	}
+}
---
+++ LudumDare40/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace LudumDare40
+{
+    public static class Program
+    {
+        [STAThread]
+        public static void Main()
+        {
+	        using (MainGame game = new MainGame())
+	        {
+		        game.Run();
+	        }
+        }
+    }
+}
